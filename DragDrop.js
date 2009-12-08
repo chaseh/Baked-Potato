a45a8054d91b352b23f36386d745b064f29219cb
chaseh <chase@hensel.com>
2009-12-07 22:31:41 -0500

    This branch pairs down the code and adds a few new files

--- DragDrop.js
+++ DragDrop.js
@@ -7,8 +7,7 @@
         eventName = [
             "drag", "dbldrag", 
             "dragstart", "dbldragstart", 
-            "dragend", "dbldragend", 
-            "qkclick", "qkdblclick", 
+            "dragend", "dbldragend",  
             "click", "dblclick"]; //names of different events
       
       function handleEvent(event) {
@@ -22,12 +21,13 @@
           case "mousemove": //mouse moving
             if (dragging !== null) {                
               if(hasDragged === true) { //are we in the middle of a drag? if so fire a drag
-                dragdrop.fire({type:eventName[clickCount], //drag
-                	target: dragging, clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
+                dragdrop.fire({type:eventName[clickCount], target: dragging, //drag
+                clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
               //if this is the start of a drag fire a `dragstart'
-              } else if (hasDragged === false && (startX != event.clientX || startY != event.clientY)) { //if the (x,y) coords have changed since mousedown then this is a drag
-                dragdrop.fire({type: eventName[2 + clickCount], //dragstart
-                	target: dragging, clientX: startX, clientY: startY, diffX: diffX, diffY: diffY});
+              } else if (hasDragged === false && (startX != event.clientX || startY != event.clientY)) { 
+                  //if the (x,y) coords have changed since mousedown then this is a drag
+                dragdrop.fire({type: eventName[2 + clickCount], target: dragging,  //dragstart
+                clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
                 hasDragged = true;
               } 
             }
@@ -35,7 +35,9 @@
 
           case "mousedown" : //mouse down
             if (target.className.indexOf("draggable") > -1) {
-              if (timeout !== null) { //check if a previous click is waiting to be executed (if so then this is a double click)
+              if (timeout !== null) { 
+                //check if a timer is waiting to be executed 
+                //(if so then this is a double click)
                 clearTimeout(timeout); 
                 timeout = null;
                 clickCount = 1;
@@ -54,17 +56,15 @@
           case "mouseup": //mouse up
             if(dragging !== null) {
               if(hasDragged === true) { // was this a drag? if so fire a dragend
-                dragdrop.fire({type: eventName[4 + clickCount], //dragend
-              	  target: target, clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
+                dragdrop.fire({type: eventName[4 + clickCount], target: target, //dragend
+                clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
                 timeout = null;
                 dragging = null;
-              } else if(hasDragged === false && (startX == event.clientX && startY == event.clientY)) { //else it must have been a click. so fire a click
-                timeout = setTimeout(function() {
-//This is where we would add a `slower' click event that only output an event fo single vs dbl clicks -- not both
-//                  dragdrop.fire({type: eventName[8 + clickCount], //click
-//                    target: dragging, clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
-                  timeout = null;
-                  dragging = null;}, 200);//quarter of a second to detect single vs. dbl click
+              } else if(hasDragged === false && (startX == event.clientX && startY == event.clientY)) { 
+                  //else it must have been a click. so fire a click
+                timeout = 
+                    setTimeout(function() { timeout = null; dragging = null;}, 250); 
+                    //quarter of a second to detect single vs. dbl click
               } else {
                 timeout = null;
                 dragging = null;
@@ -72,20 +72,19 @@
             }
             break;
             
-          case "click": //quick click
-            if(dragging !== null) {
-	          	dragdrop.fire({type: eventName[6], //quickclick
-    	          target: dragging, clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
-    	    }
+          case "click":
+            if(hasDragged === false) {
+  	      	  dragdrop.fire({type: eventName[6], target: target, //click
+	            clientX: event.clientX, clientY: event.clientY});
+            }
             break;
             
           case "dblclick":
-            if(dragging !== null) {
-	          	dragdrop.fire({type: eventName[7], //quickdbldclick
-    	          target: dragging, clientX: event.clientX, clientY: event.clientY, diffX: diffX, diffY: diffY});
-    	    }
+            if (hasDragged === false) {
+              dragdrop.fire({type: eventName[7], target: target, //quickdblclick
+  	            clientX: event.clientX, clientY: event.clientY});
+            }
     	    break;
-
         }
       };
       
--- dd.js
+++ dd.js
@@ -1,109 +1,95 @@
-   
-	function min(val1, val2) {
-	  return val2 > -1 ? (val1 > val2 ? val2 : val1) : val1;
-	}
+function min(val1, val2) {
+	return val2 > -1 ? (val1 > val2 ? val2 : val1) : val1;
+}
 	
-	function charCodeIndexOf(val, code) {
-	  for(var i = 0, len = val.length; i < len; i++) {
-	    if(val.charCodeAt(i) == code) {
-	      return i;
+function charCodeIndexOf(val, code) {
+	for(var i = 0, len = val.length; i < len; i++) {
+		if(val.charCodeAt(i) == code) {
+			return i;
 	    }
-	  }
-	  return -1;
 	}
+	return -1;
+}
 
-	function charCodeLastIndexOf(val, code) {
-	  
-	  for(var i = val.length - 1; i >= 0; i--) {
+function charCodeLastIndexOf(val, code) {
+	for(var i = val.length - 1; i >= 0; i--) {
 	    if(val.charCodeAt(i) == code) {
-	      return i;
+	    	return i;
 	    }
-	  }
-	  return val.length;
 	}
+	return val.length;
+}
 	
-	function countCharCode(val, code) {
-	  ans = 0;
-	  for(var i = 0, len = val.length; i < len; i++) {
+function countCharCode(val, code) {
+	ans = 0;
+	for(var i = 0, len = val.length; i < len; i++) {
 	    if(val.charCodeAt(i) == code) {
-	      ans++;
+	        ans++;
 	    }
-	  }
-	  return ans;
 	}
+	return ans;
+}
 
-	EventUtil.addHandler(document, "keydown", function(event) {
-	  event = EventUtil.getEvent(event);
-	  var target = EventUtil.getTarget(event), 
-	    val = target.value, code = EventUtil.getCharCode(event);
-	  if(val.length == 0 && code == 8) { //pressed backspace with no characters
-	    document.body.removeChild(target);
-	    target = null;
-	  } 
-	});
-	
-	EventUtil.addHandler(document, "keyup", function(event) {
-	  event = EventUtil.getEvent(event);
-	  var target = EventUtil.getTarget(event), 
-	    val = target.value, code = EventUtil.getCharCode(event);
-	  target.cols = 1 + Math.round(.8 * min(val.length, charCodeIndexOf(val, 10)));
-	  target.rows = 1 + countCharCode(val, 10);
-/*	    if(val.length - charCodeLastIndexOf(val, 10) > target.cols) {
-	      var space = charCodeLastIndexOf(val, 32);
-	      if((val.length - space) < target.cols) {
-	        alert(code);
-	        val = val.slice(0, space).concat("<br />", val.slice(space + 1, val.length));
-	      } else {
-	        val.concat("<br />");
-	      }
-	    }*/
-	});
-	
-    DragDrop.enable();       
+EventUtil.addHandler(document, "keyup", function(event) {
+	event = EventUtil.getEvent(event);
+	var target = EventUtil.getTarget(event), 
+		val = target.value, code = EventUtil.getCharCode(event);
+	target.cols = 1 + min(val.length, charCodeIndexOf(val, 10));
+	target.rows = 1 + countCharCode(val, 10);
+});
+
+//function to garbage collect the empty text areas
+//the function observes that if we check for garbage after every click, 
+//at most one empty text area can exist
+//as a result we can only check if the last if the text area is empty
+//and if so delete it   	
+function gc(target) {
+	try {
+		var tas = document.getElementsByTagName("textarea"), pos = tas.length - 1;
+		if(tas[pos].value.length == 0 && tas[pos] !== target) { 
+	    	document.body.removeChild(tas[pos]);
+	    	tas[pos] = null;    
+		}
+	} catch (ex) {} //fails if their are no text area's
+}
+
+//var focused = null; //the DOMobject that currently has focus
+DragDrop.enable();       
     
-    DragDrop.addHandler("qkclick", function(event) {
-      var target = event.target;
-      switch(target.tagName.toLowerCase()) {
-       case "textarea" :
-            var body = document.body;
-         	body.removeChild(target); body.appendChild(target);
-         	target.focus();
-         	break;
+DragDrop.addHandler("click", function(event) {
+	var target = event.target, body = document.body;
+	gc(target);
+    switch(target.tagName.toLowerCase()) {
+    /*    case "textarea" :
+        	alert("here");
+       		if(focused.isNot(target)) {
+	         	body.removeChild(target); 
+	         	body.appendChild(target);
+    	     	target.focus();
+    	     	focused = target;
+    	    }
+         	break;*/
         case "body" :
-			var div = document.createElement("textarea");
-			div.type = "html";
-            div.cols ="1";
-            div.rows ="1";
-         	div.className = "box draggable";
-         	div.style.left = event.clientX + "px";
-         	div.style.top = event.clientY + "px";
-         	document.body.appendChild(div);
-         	div.focus();
-         	break;
-  
+        case "html" : // needed
+        	var text = new TextBox(event.clientX, event.clientY);
+			//focused = text;
+			text.addToDOM();
+			text.focus();
+         	break;  
      }
   });
   
-    DragDrop.addHandler("qkdblclick", function(event) {
-      var target = event.target;
-      switch(target.tagName.toLowerCase()) {
-       case "textarea" :
-      		document.body.removeChild(event.target);
-      		target = null;
-         	break;
-     }
-    });
-
-    var dbldragger = function(event) {
-      var target = event.target, body = document.body;
-      switch(target.tagName.toLowerCase()) {
-        case "textarea" :
-          target.style.left = (event.clientX - event.diffX) + "px"; 
-          target.style.top = (event.clientY - event.diffY) + "px";
-          break;
-      }
+var dbldragger = function(event) {
+	var target = event.target;
+    switch(target.tagName.toLowerCase()) {
+    	case "textarea":
+    		//focused = target;
+          	target.style.left = (event.clientX - event.diffX) + "px"; 
+          	target.style.top = (event.clientY - event.diffY) + "px";
+          	break;
     }
+}
     
-    DragDrop.addHandler("dbldragstart", dbldragger);
-    DragDrop.addHandler("dbldrag", dbldragger);
-    DragDrop.addHandler("dbldragend", dbldragger);
+DragDrop.addHandler("dbldragstart", dbldragger);
+DragDrop.addHandler("dbldrag", dbldragger);
+DragDrop.addHandler("dbldragend", dbldragger);
