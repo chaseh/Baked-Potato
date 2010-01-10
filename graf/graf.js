c4713fb984c1c81bffa01b20517469e47856b40e
Alex Komoroske <jkomoros@gmail.com>
2010-01-10 15:12:00 -0500

    Made it so that you can add circles by hitting a button.

--- graf/graf.js
+++ graf/graf.js
@@ -15,3 +15,8 @@ graf.App = function() {
 	this.circle.setTransformation(0,0,100,100,200);
 	this.graphics.render(document.getElementById("surface"));
 }
+
+graf.App.prototype.addCircle = function() {
+	var circle = this.graphics.drawEllipse(Math.random() * this.graphics.getSize().width, Math.random() * this.graphics.getSize().height, 30, 30, new goog.graphics.Stroke(1, "#F00"), new goog.graphics.SolidFill("#000"));
+	this.graphics.render(document.getElementbyId("surface"));
+}
