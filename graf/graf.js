00615b91934d92d513a032e71b996b958214291a
Alex Komoroske <jkomoros@gmail.com>
2010-01-10 14:44:44 -0500

    Made it so that we actually draw an ellipse on the screen.

--- graf/graf.js
+++ graf/graf.js
@@ -4,8 +4,12 @@
 
 goog.provide("graf.App");
 
-goog.require("graf.Surface");
+goog.require("goog.graphics")
 
 graf.App = function() {
-	this.surface = graf.Surface(this);
+	this.graphics = goog.graphics.createGraphics(400,400);
+	var blackFill = new goog.graphics.SolidFill("#000");
+	var redStroke = new goog.graphics.Stroke(1, "#F00");
+	this.circle = this.graphics.drawEllipse(100,200, 10, 20, redStroke, blackFill);
+	this.graphics.render(document.getElementById("surface"));
 }
