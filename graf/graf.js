18f11e2e3863c2c87b1eadaf917f7c85e63af963
Alex Komoroske <jkomoros@gmail.com>
2010-01-10 15:50:18 -0500

    Made it so circles rotate out of existance.

--- graf/graf.js
+++ graf/graf.js
@@ -28,6 +28,7 @@ graf.App.prototype.decay = function() {
 		circle = this.circles[i];
 		if (circle.getWidth() <= 0) continue;
 		circle.setSize(circle.getWidth() - 1, circle.getHeight() - 1);
+		circle.setRotation(circle.getRotation() + 10);
 	}
 }
 
@@ -36,8 +37,8 @@ graf.App.prototype.addCircle = function() {
 	var circle = new goog.graphics.ext.Ellipse(this.graphicsGroup);
 	circle.setLeft(Math.random() * this.graphics.getWidth());
 	circle.setTop(Math.random() * this.graphics.getHeight());
-	circle.setHeight(30);
-	circle.setWidth(30);
+	circle.setHeight(Math.random() * 100);
+	circle.setWidth(Math.random() * 50);
 	circle.setFill(new goog.graphics.SolidFill("#000"));
 	circle.setStroke(new goog.graphics.Stroke(1, "#F00"));
 	this.circles.push(circle);	
